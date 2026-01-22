39233c7d4e261e89a3b4bf3a91b6ce5680abee5d
kumahajime-cloud <hajimeazb@gmail.com>
2026-01-22 18:27:08 +0900

    Fix: Asset precompilation for production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,22 +1,12 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-//= require rails-ujs
-//= require turbolinks
-//= require_tree .
-
-document.addEventListener("turbolinks:load", function() {
+// OshiConnect JavaScript
+document.addEventListener("DOMContentLoaded", function() {
   // いいねボタンの処理
   document.querySelectorAll('.like-button').forEach(function(button) {
     button.addEventListener('click', function(e) {
       e.preventDefault();
       var form = this.closest('form');
+      if (!form) return;
+      
       var url = form.action;
       var method = form.querySelector('input[name="_method"]') ? 
                    form.querySelector('input[name="_method"]').value : form.method;
